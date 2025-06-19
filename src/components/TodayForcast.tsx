112633c1766410be7a407d61b9356aaa14a749ab
krishrana1835 <krishrana1835@gmail.com>
2025-06-19 20:18:02 +0530

    new

--- src/components/TodayForcast.tsx
+++ src/components/TodayForcast.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { fetchTodayForecast } from "../api/weatherApi";
-import { getDayOrNight } from "./WeatherDisp";
 
 interface Props {
   lat: number;
