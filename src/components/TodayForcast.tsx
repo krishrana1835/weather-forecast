a896ec03d1e2717f34a93bcade054995e3f351dc
krishrana1835 <krishrana1835@gmail.com>
2025-06-19 20:01:34 +0530

    day night image solve

--- src/components/TodayForcast.tsx
+++ src/components/TodayForcast.tsx
@@ -2,28 +2,29 @@ import { useEffect, useState } from "react";
 import { fetchTodayForecast } from "../api/weatherApi";
 import { getDayOrNight } from "./WeatherDisp";
 
-interface Props{
+interface Props {
   lat: number;
   lon: number;
   data: any;
 }
 
-function TodayForecast({lat, lon, data}: Props) {
+function TodayForecast({ lat, lon, data }: Props) {
+  const [weatherData, setWeatherData] = useState<
+    { time: string; weather: string; temperature: number }[]
+  >([]);
 
-  const [weatherData, setWeatherData] = useState<{time: string, weather: string, temperature: number}[]>([])
-
-  useEffect(()=> {
-    const fetchData = async () =>{
-      try{
-        const result = await fetchTodayForecast(lat,lon)
-        setWeatherData(result)
-      }catch(error){
-        console.log("5-days Forcast error", error)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fetchTodayForecast(lat, lon);
+        setWeatherData(result);
+      } catch (error) {
+        console.log("5-days Forcast error", error);
       }
-    }
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <div className="w-100 component-bg todayforcast p-3">
@@ -31,8 +32,21 @@ function TodayForecast({lat, lon, data}: Props) {
       <div className="forecast-row">
         {weatherData.map((info, index) => (
           <div key={index} className="forecast-item text-center">
-            <p className="info m-0">{info.time.split(" ")[1].split(":")[0]}:00</p>
-            <img src={`/${data.weather[0].main}${getDayOrNight(data)}.png`} alt="" height="50" width="50" className="my-2" />
+            <p className="info m-0">
+              {info.time.split(" ")[1].split(":")[0]}:00
+            </p>
+            <img
+              src={`/${data.weather[0].main}${
+                parseInt(info.time.split(" ")[1].split(":")[0], 10) > 6 &&
+                parseInt(info.time.split(" ")[1].split(":")[0], 10) < 19
+                  ? "day"
+                  : "night"
+              }.png`}
+              alt=""
+              height="50"
+              width="50"
+              className="my-2"
+            />
             <p className="info m-0">{info.temperature}°</p>
           </div>
         ))}
